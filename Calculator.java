6261231f496bda0634dd77b695691b6169f11c24
mohan <mohanrajcom7@gmail.com>
2025-01-19 13:47:40 +0530

    adding mul and div methods

--- Calculator.java
+++ Calculator.java
@@ -9,7 +9,18 @@ public class Calculator {
     public static double subtract(double a, double b) {
         return a - b;
     }
-    
+    public static double multiply(double a, double b) {
+        return a * b;
+    }
+
+    // Division method
+    public static double divide(double a, double b) {
+        if (b == 0) {
+            System.out.println("Error! Division by zero.");
+            return Double.NaN; // Not a Number
+        }
+        return a / b;
+    }
     
 
 public static void main(String[] args) {
